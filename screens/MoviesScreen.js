908684f2cee096e298ea5fda4a41b9e154512e8a
StijnVP <stijnvanpeborgh@gmail.com>
2022-12-05 13:05:05 +0100

    upcomming movies

---
+++ screens/MoviesScreen.js
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, FlatList, TextInput } from 'react-native';
+
+import apiKey from '../apiKey';
+const MoviesScreen = ({ navigation }) => {
+
+  const [movies, setMovies] = useState([]);
+
+  const getUpcommingMovies = async () => {
+    try {
+      const response = await fetch("https://moviesminidatabase.p.rapidapi.com/movie/order/byRating/", {
+        "method": "GET",
+        "headers": {
+          "x-rapidapi-host": "moviesminidatabase.p.rapidapi.com",
+          "x-rapidapi-key": apiKey
+        }
+      })
+      const json = await response.json();
+      console.log(json);
+      setMovies(json.results);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getUpcommingMovies();//laad upcomming movies wanneer scherm laadt
+  }, []);
+
+
+  return (
+    <View style={styles.screen}>
+      <FlatList
+        data={movies}
+        keyExtractor={item => item.imdb_id}//gebruik imdb_id als key voor de flatlist
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
+      />
+    </View >
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 50,
+  }
+});
+export default MoviesScreen;
