b85f17a9e57d8491aa19735b599529cf5143958f
StijnVP <stijnvanpeborgh@gmail.com>
2022-12-05 13:40:25 +0100

    search by title

--- screens/MoviesScreen.js
+++ screens/MoviesScreen.js
@@ -29,9 +29,34 @@ const MoviesScreen = ({ navigation }) => {
     getUpcommingMovies();//laad upcomming movies wanneer het scherm laadt
   }, []);
 
+  const getMoviesByTitleSearch = async (enteredText) => {//argument meegegeven door onChangeText
+    try {
+      if (enteredText.length > 3) {
+        const url = encodeURI("https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/" + enteredText + "/");
+        console.log(url);
+        const response = await fetch(url, {
+          "method": "GET",
+          "headers": {
+            "x-rapidapi-host": "moviesminidatabase.p.rapidapi.com",
+            "x-rapidapi-key": apiKey
+          }
+        })
+        const json = await response.json();
+        console.log(json);
+        setMovies(json.results);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
     <View style={styles.screen}>
+      <TextInput
+        placeholder="search movie"
+        style={styles.input}
+        onChangeText={getMoviesByTitleSearch}//geeft argument enteredText mee, denk aan de taskInputHandler uit de todo app.
+      />
       <FlatList
         data={movies}
         keyExtractor={item => item.imdb_id}//gebruik imdb_id als key voor de flatlist
